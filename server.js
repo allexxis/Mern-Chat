92fec919a875024eaf225d48e44d6c9eaf5ce51a
allexxis <gabr.933@gmail.com>
2018-11-09 20:28:20 -0600

    Mongo keys added

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
-const express = require("express")
-const mongoose = require("mongoose")
-const bodyParser = require("body-parser")
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
-const app = express()
+const app = express();
 
 //Body-Parser Middleware
+app.use(bodyParser.json());
+
+//DB Config
+const db = require('./config/keys').mongoURI;
+
+//Connect to Mongo
+mongoose
+    .connect(db)
+    .then(()=>console.log('Mongo connected'))
+    .catch(err=>console.log(err));
+
+const port = 5000;
+
+app.listen(port,()=>console.log(`Server started on ${port}`));
