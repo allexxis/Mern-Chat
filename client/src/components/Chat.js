5060e3addf4f2b8a1cab8a775e839897627cb2e1
allexxis <gabr.933@gmail.com>
2018-11-10 13:46:13 -0600

    chat test

--- client/src/components/Chat.js
+++ client/src/components/Chat.js
@@ -2,6 +2,8 @@ import React ,{Component} from 'react';
 import Grid from '@material-ui/core/Grid'
 import TextField from '@material-ui/core/TextField'
 import Card from '@material-ui/core/Card'
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
 
 export default class Chat extends Component {
     constructor(props){
@@ -17,7 +19,14 @@ export default class Chat extends Component {
     render() {
         return (
             <Grid className='chat-container' container direction="row">
-                
+                <Paper elevation={1}>
+                    <Typography variant="h5" component="h3">
+                        {this.state.messages[0].username}
+                    </Typography>
+                    <Typography component="p">
+                        {this.state.messages[0].content}
+                    </Typography>
+                </Paper>
             </Grid>
         )
     }
