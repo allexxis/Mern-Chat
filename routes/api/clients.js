26b47775e57ea73836a6869d6190e59c833400f5
allexxis <gabr.933@gmail.com>
2018-11-09 23:18:44 -0600

    React Client added

--- routes/api/clients.js
+++ routes/api/clients.js
@@ -5,14 +5,38 @@ const router = express.Router();
 const Client = require('../../models/Client');
 
 // @route GET api/clients
-// @desc  GET ALL clients
+// @desc  Get all Clients
 // @acces  Public 
 
 router.get('/',(req,res)=>{
     Client
-        .find()
-        .sort({date:-1})
-        .then(clients=>res.json(clients))
+    .find()
+    .sort({date:-1})
+    .then(clients=>res.json(clients))
 })
+
+// @route POST api/clients
+// @desc  Create a Client
+// @acces  Public 
+
+router.post('/',(req,res)=>{
+    const newClient = new Client({
+        alias: req.body.alias
+    });
+    newClient.save().then(client=>res.json(client))
+})
+
+// @route DELTE api/clients/:id
+// @desc  Delete a Client
+// @acces  Public 
+
+router.delete('/:id',(req,res)=>{
+    Client
+    .findById(req.params.id)
+    .then(client=>client
+        .remove()
+        .then(()=>res.json({succes:true})))
+        .catch(err=>res.json({succes:false}));
+});
 
 module.exports = router; 
